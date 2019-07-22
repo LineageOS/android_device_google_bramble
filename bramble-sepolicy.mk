1998e3777b84fe8346d47c1eb1cdc0ed9d04efab
Roger Liao <rogerliao@google.com>
2019-07-22 11:57:43 +0800

    Initialize bramble sepolicy
    Bug: 138083938
    Bug: 137166127
    Change-Id: I467f0d2197bdef15b921535f21c5b50a00a7b762

---
+++ bramble-sepolicy.mk
@@ -0,0 +1,8 @@
+ARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/bramble-sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/bramble-sepolicy/private
+
+# vendors
+BOARD_SEPOLICY_DIRS += device/google/bramble-sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/bramble-sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/google/bramble-sepolicy/vendor/qcom/sm7250
+
