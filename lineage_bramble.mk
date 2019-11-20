9e30695e52158324b18a7663acaf208ddc6b2655
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    bramble: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ lineage_bramble.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/bramble/aosp_bramble.mk)
+
+include device/google/bramble/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4a (5G)
+PRODUCT_NAME := lineage_bramble
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=bramble \
+    PRIVATE_BUILD_DESC="bramble-user 12 SP2A.220305.012 8177914 release-keys"
+
+BUILD_FINGERPRINT := google/bramble/bramble:12/SP2A.220305.012/8177914:user/release-keys
