1d380ec81276437edce7f90fee87d5faeb68795c
Eamon Powell <eamonpowell@outlook.com>
2020-03-17 20:50:51 +1100

    bramble: Don't copy aosp_excluded_hardware.xml to vendor
    This will only hurt GPS accuracy. Let's just drop this.
    
    Change-Id: I9ae662e47b4bf8d9506f3938d9f4217acf947dc2

--- aosp_bramble.mk
+++ aosp_bramble.mk
@@ -47,8 +47,8 @@ $(call inherit-product-if-exists, vendor/google_devices/bramble/proprietary/devi
 $(call inherit-product-if-exists, vendor/google_devices/bramble/prebuilts/device-vendor-bramble.mk)
 
 # Exclude features that are not available on AOSP devices.
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
 
 # Don't build super.img.
 PRODUCT_BUILD_SUPER_PARTITION := false
