af229252350be0633cd2187e9cb4d5920154e65c
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    bramble: do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,15 +6,6 @@
 
 $(call inherit-product, device/google/redbull/device-lineage.mk)
 
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.device-V2-ndk.vendor:64 \
-    android.hardware.camera.provider-V2-ndk.vendor:64
-
 # HBM
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlay
-
-# Wi-Fi
-PRODUCT_PACKAGES += \
-    android.hardware.wifi-V2-ndk.vendor:64
